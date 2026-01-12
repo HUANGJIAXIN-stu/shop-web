ff676bfbd2b499a7e77f638b2f18569d6e782fb5
Ma Yuejingran <kritt.hub01@gmail.com>
2026-01-13 01:42:03 +0800

    Add backend model classes and database utilities

---
+++ src/utils/DBUtil.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/cake";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
